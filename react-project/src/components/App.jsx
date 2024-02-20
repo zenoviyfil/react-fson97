7c7b119c8b096084d98f40493ff6db9165d7cfbb
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-02-20 16:10:12 +0100

    fresh start

---
+++ react-project/src/components/App.jsx
@@ -0,0 +1,31 @@
+import { Product } from "./Product";
+
+const productData = [{
+  id:"1_product",
+  name:"Tacos With Lime",
+  imgUrl:"<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>",
+  price:"10.99"
+},{
+  id:"2_product",
+  name:"Fries and Burger",
+  imgUrl:"<https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640>",
+  price:"14.29"
+}]
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+      {productData.map((item) => {
+        return (
+          <Product 
+          key={item.id}
+          name={item.name}
+          imgUrl={item.imgUrl}
+          price={item.price}
+          />
+        )
+      })}
+    </div>
+  );
+}
